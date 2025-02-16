d6ac3f79d74b32b3c184c6492d3220959e760e10
Ilya Borisov <battersman27@gmail.com>
2025-02-16 20:30:07 +0300

    RAZRABOTKA-61: Баг: кривой триггер отображения карточек в разделе всех women/men

--- components/card/index.vue
+++ components/card/index.vue
@@ -43,46 +43,50 @@
     ref="cardRef"
     :to="localePath(profileLink)"
     class="card"
-    :class="[
-      isBeenShown && '!opacity-100 !translate-y-0'
-    ]"
     @mouseenter="isHovered = true"
     @mouseleave="isHovered = false"
   >
-    <div class="overflow-hidden relative h-[403px] max-mobile:h-[232px] max-tablet:h-[327px]">
-      <template v-if="params.acf.preview_image">
-        <img
-          class="card__cover"
-          :src="params.acf.preview_image[0]?.sizes.large"
-          :alt="getFullName"
-        >
-        <img
-          v-if="params.acf.preview_image[1]"
-          class="card__cover"
-          :class="[
-            'opacity-0 absolute top-0 left-0 ',
-            isHovered && 'opacity-100 !scale-110',
-          ]"
-          :src="params.acf.preview_image[1]?.sizes.large"
-          :alt="getFullName"
-        >
-      </template>
-      <template v-else>
-        <div class="w-full h-full bg-accent" />
-      </template>
-    </div>
-    <div class="card__footer">
-      <UiButton
-        :label="params.acf.fio[locale].name"
-        :size="nameButtonSize"
-        :is-underline-hover="!isHovered"
-        :is-underline="isHovered"
-        is-uppercase
-      />
-      <div
-        class="card__lastname"
-        v-text="params.acf.fio[locale].last_name"
-      />
+    <div
+        class="card__content"
+        :class="[
+          isBeenShown && '!opacity-100 !translate-y-0'
+        ]"
+    >
+      <div class="overflow-hidden relative h-[403px] max-mobile:h-[232px] max-tablet:h-[327px]">
+        <template v-if="params.acf.preview_image">
+          <img
+            class="card__cover"
+            :src="params.acf.preview_image[0]?.sizes.large"
+            :alt="getFullName"
+          >
+          <img
+            v-if="params.acf.preview_image[1]"
+            class="card__cover"
+            :class="[
+              'opacity-0 absolute top-0 left-0 ',
+              isHovered && 'opacity-100 !scale-110',
+            ]"
+            :src="params.acf.preview_image[1]?.sizes.large"
+            :alt="getFullName"
+          >
+        </template>
+        <template v-else>
+          <div class="w-full h-full bg-accent" />
+        </template>
+      </div>
+      <div class="card__footer">
+        <UiButton
+          :label="params.acf.fio[locale].name"
+          :size="nameButtonSize"
+          :is-underline-hover="!isHovered"
+          :is-underline="isHovered"
+          is-uppercase
+        />
+        <div
+          class="card__lastname"
+          v-text="params.acf.fio[locale].last_name"
+        />
+      </div>
     </div>
   </NuxtLink>
 </template>
@@ -93,11 +97,15 @@
       relative
       max-w-[334px]
       max-mobile:max-w-[175px]
-      w-full
-      opacity-0
-      translate-y-1/4
-      duration-700
-      transition;
+      w-full;
+
+    &__content {
+      @apply
+        translate-y-1/4
+        opacity-0
+        duration-700
+        transition;
+    }
 
     &__cover {
       @apply absolute top-0 left-0 transition duration-500 aspect-square w-full h-full object-cover object-top;
